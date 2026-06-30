71d8f50de1be2b0476cff43e7a0ae5cf5e01df49
Jamescreator0102 <krucreator@gmail.com>
2026-06-30 15:19:41 +0700

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,1 +1,34 @@
+let cameraStream = null;
 
+async function openCamera() {
+  const video = document.getElementById("camera");
+
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    throw new Error("อุปกรณ์นี้ไม่รองรับการเปิดกล้อง");
+  }
+
+  cameraStream = await navigator.mediaDevices.getUserMedia({
+    video: {
+      facingMode: "user",
+      width: { ideal: 960 },
+      height: { ideal: 540 }
+    },
+    audio: false
+  });
+
+  video.srcObject = cameraStream;
+
+  return new Promise(resolve => {
+    video.onloadedmetadata = () => {
+      video.play();
+      resolve(true);
+    };
+  });
+}
+
+function closeCamera() {
+  if (cameraStream) {
+    cameraStream.getTracks().forEach(track => track.stop());
+    cameraStream = null;
+  }
+}
